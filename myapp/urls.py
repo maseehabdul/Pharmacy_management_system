ee210d3ee7a1677a73cfcfeb83bc1f3439bc202f
maseehabdul <abdulmaseeh812@gmail.com>
2024-01-02 22:04:02 +0530

    updated pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,7 @@ from myapp import views
 
 urlpatterns=[
     path('',views.index,name='index'),
-      path('index',views.login,name='login'),
-    path('login',views.login,name='login'),
-      path('login/',views.login,name='login'),
+      path('index',views.index,name='index'),
+       path('login',views.login,name='login')
     
 ]
