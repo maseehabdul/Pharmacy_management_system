380ac0a79ce8b542de2dae5d6a1b0cc4d96e11d2
maseehabdul <abdulmaseeh812@gmail.com>
2024-01-02 21:54:49 +0530

    udated url

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,8 @@ from myapp import views
 
 urlpatterns=[
     path('',views.index,name='index'),
-    path('login/',views.login,name='login')
+      path('index',views.login,name='login'),
+    path('login',views.login,name='login'),
+      path('login/',views.login,name='login'),
+    
 ]
