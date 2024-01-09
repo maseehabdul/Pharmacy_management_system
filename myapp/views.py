121bf0e409923b511aaedb589ce76b818e0293e3
maseehabdul <abdulmaseeh812@gmail.com>
2024-01-09 21:39:54 +0530

    login updated

--- myapp/views.py
+++ myapp/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
 def login(requests):
     if request.method == 'POST':
        username = request.POST['username']
